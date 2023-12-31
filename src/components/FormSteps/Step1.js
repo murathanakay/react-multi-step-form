7faa8ddd9caa26904f537243872019157c208394
murathanakay <murathanna@gmail.com>
2023-12-31 17:52:31 +0300

    add phone number and gender selects

--- src/components/FormSteps/Step1.js
+++ src/components/FormSteps/Step1.js
@@ -1,18 +1,27 @@
 import React from "react";
-import {TextInput} from "../FormFields";
+import {PhoneInput, RadioGroup, TextInput} from "../FormFields";
 
 import * as Yup from "yup";
+// import "yup-phone";
 
 const Step1 = (props) => {
+  const genderOptions = ["Male", "Female", "Rather not to say"];
   return (
     <>
-      <div className="grid grid-cols-2 gap-4">
-        <div className="">
-          <TextInput label="First Name*" name="First Name" />
-        </div>
-        <div className="">
-          <TextInput label="Last Name*" name="Last Name" />
-        </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 md:gap-5">
+        <TextInput label="First Name*" name="First Name" />
+        <TextInput label="Last Name*" name="Last Name" />
+      </div>
+      <div className="grid grid-cols-1 md:gap-5">
+        <RadioGroup name="Gender" options={genderOptions} />
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 md:gap-5">
+        <PhoneInput
+          name="Phone Number"
+          label="Phone Number"
+          handleUpdateSchema={props.handleUpdateSchema}
+        />
+        <TextInput label="E-mail" name="E-mail" />
       </div>
     </>
   );
@@ -26,11 +35,26 @@ const Step1Form = {
   },
 
   validationSchema: {
-    "First Name": Yup.string().required("First Name is required."),
+    "First Name": Yup.string()
+      .min("2", "too short")
+      .required("First Name is required."),
+    "Last Name": Yup.string()
+      .min("2", "too short")
+      .required("Last Name is required."),
+    "E-mail": Yup.string().email().required("E-mail is required."),
+
+    Gender: Yup.string()
+      // .oneOf(["Male", "Female"])..etc
+      .required("Must select one of the above."),
+    "Phone Number": Yup.string(),
   },
 
   initialValues: {
     "First Name": "",
+    "Last Name": "",
+    "E-mail": "",
+    Gender: "",
+    "Phone Number": "",
   },
 };
 
